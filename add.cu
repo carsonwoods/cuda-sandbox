be3e251e9f57dd264a78f20999867b1e1f7cea9d
Carson Woods <cwoods289@gmail.com>
2018-07-09 10:44:46 -0400

    Add CUDA Implementation

---
+++ add.cu
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+
+__global__
+void add(int n, float *x, float *y) {
+    int z = 0;
+    for (int i = 0; i < n; i++) {
+        y[i] = x[i] + y[i];
+        z++;
+    }
+}
+
+int main() {
+
+    //For my own sanity lets explain this.
+    //1<<20 is a notation that in this context represents
+    //a bitshift. That means that you have the bit 1 and then you shift it to the
+    //(in this case) left by 20 spaces and fill the empty space with zeros.
+    int N = 1<<25; // 1M elements
+
+    float *x, *y;
+
+    //Allocates "Unified Memory" which is accessible from both the CPU and GPU.
+    cudaMallocManaged(&x, N*sizeof(float));
+    cudaMallocManaged(&y, N*sizeof(float));
+
+
+    //initialize x and y arrays on the host
+    for (int i = 0; i < N; i++) {
+        x[i] = 1.0f;
+        y[i] = 2.0f;
+    }
+
+    //Runs cuda kernel on 1M elements on the CPU
+    add<<<1, 1>>>(N, x, y);
+
+    //Forces CPU to wait for GPU to finish before accessing
+    cudaDeviceSynchronize();
+
+    // Check for errors (all values should be 3.0f)
+    float maxError = 0.0f;
+    for (int i = 0; i < N; i++) {
+        maxError = fmax(maxError, fabs(y[i]-3.0f));
+    }
+    std::cout << "Max error: " << maxError << std::endl;
+
+    // Free memory
+    cudaFree(x);
+    cudaFree(y);
+
+    return 0;
+}
