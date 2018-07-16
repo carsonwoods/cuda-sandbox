aa7863318d33e73a8d6920e51386f037f93cf0c0
Carson Woods <cwoods289@gmail.com>
2018-07-16 11:26:35 -0500

    Add CUDA kernel for add2()

--- add.cu
+++ add.cu
@@ -11,6 +11,20 @@ void add(int n, float *x, float *y) {
     }
 }
 
+
+//I don't relly know what to call this function so this describes its
+//functionality and it will be named add2 because its basically add + extra stuff
+//z = alpha*x + beta* y
+//where z, x, and y are vectors of length N, and alpha and beta are scalars.
+__global__
+void add2(int n, float *x, float *y, float a, float b) {
+     float *z = new float[n];
+     for (int i = 0; i < n; i++) {
+         z[i] = (a * x[i]) + (b * y[i]);
+     }
+}
+
+
 int main() {
 
     //For my own sanity lets explain this.
@@ -22,13 +36,14 @@ int main() {
     float *x, *y;
 
     //Allocates "Unified Memory" which is accessible from both the CPU and GPU.
-    cudaError_t err = cudaMallocManaged(&x, N*sizeof(float));
-    if (err != cudaSuccess) {
-	cout << "CUDA Error" << endl;
-   	//printf("%s\n", cudaGetErrorString(err));
-    }	 
-   // cudaMallocManaged(&x, N*sizeof(float));
-    cudaMallocManaged(&y, N*sizeof(float));
+    cudaError_t cudaMallocErr1 = cudaMallocManaged(&x, N*sizeof(float));
+    if (cudaMallocErr1 != cudaSuccess) {
+	    cout << "CUDA Error" << endl;
+    }
+    cudaError_t cudaMallocErr2 = cudaMallocManaged(&y, N*sizeof(float));
+    if (cudaMallocErr2 != cudaSuccess) {
+        cout << "CUDA Error" << endl;
+    }  	 
 
     //initialize x and y arrays on the host
     for (int i = 0; i < N; i++) {
@@ -36,15 +51,18 @@ int main() {
         y[i] = 2.0f;
     }
 
-    //Runs cuda kernel on 1M elements on the CPU
+    //Runs cuda kernel on 1M elements on the GPU
     add<<<1, 1>>>(N, x, y);
+    add2<<<1, 1>>>(N, x, y, 4.0, 5.0);
 
-    cout << "Add Completed" << endl;
+    cout << "Done!" << endl;
 
     //Forces CPU to wait for GPU to finish before accessing
     cudaDeviceSynchronize();
 
     // Check for errors (all values should be 3.0f)
+    //NOTE: This does not work for any function other than add()
+    //NOTE: This is a massive performance hit. Will eventually delegate to GPU.
     float maxError = 0.0f;
     for (int i = 0; i < N; i++) {
         maxError = fmax(maxError, fabs(y[i]-3.0f));
