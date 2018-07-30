e1f2c6b628429f380a2eb358687f82aff4c9ff35
Carson Woods <cwoods289@gmail.com>
2018-07-30 10:41:44 -0400

    Clean up redundant code

--- add_blocks.cu
+++ add_blocks.cu
@@ -1,9 +1,7 @@
 #include <iostream>
-#include <math.h>
 #include <stdio.h>
 
 using namespace std;
-
 
 __global__ void testPrint() {
     printf("Hello world\n");
@@ -22,7 +20,6 @@ void add(int n, float *x, float *y) {
     }
 
 }
-
 
 //I don't relly know what to call this function so this describes its
 //functionality and it will be named add2 because its basically add + extra stuff
@@ -43,147 +40,36 @@ void add2(int n, float *x, float *y, float a, float b) {
     }
 }
 
-__global__ void verticalOperation(int size, float *deviceArray, float *deviceResult) {
-
-    /*
-    //each thread loads one element from global memory into shared memory
-    int thread_id = threadIdx.x;
-    int numBlocks = gridDim.x;
-    int numTotalThreads = gridDim.x * blockDim.x;
-    int index = blockIdx.x * blockDim.x + threadIdx.x;
-
-    extern __shared__ float thread_maxima[];
-
-    //Sets each thread's starting point in the global_input_data
-    int loopIndex = (size/numTotalThreads) + index;
-
-    //sets thread_max to first element in array for initial comparison
-    float thread_max = global_input_data[loopIndex];
-
-    //iterate across a threads domain until maximum is found
-    //Loop operates as follows:
-    //Each thread has its starting position set by loopIndex.
-    //Loop will iterate until the starting point of the next thread is hit.
-    //Each iteration the thread compares the value in the array against the current thread_max
-    for(int i = loopIndex; i < loopIndex + size/blockDim.x; i++) {
-        if (global_input_data[loopIndex + i] > thread_max) {
-            thread_max = global_input_data[loopIndex + i];
-        }
-    }
-
-    //max for each thread is placed into thread_maxmia for next comparison
-    //NOTE: Since thread_maxima is shared memory thread_id is used.
-    //      Shared Memory is only shared across a single block so index isn't used.
-    thread_maxima[thread_id] = thread_max;
-
-    //Threads are synced to ensure all comparisons that need to be made are done.
-    __syncthreads();
-
-    //find the maximum value from all threads in a single block
-    //appends that value to block_maximum[]
-    if (thread_id == 0) {
-        float max = thread_maxima[0];
-        //thread 0 of each block iterates across all values of thread_maxima
-        //numTotalThreads/(size/blockDim.x) should be the size of thread_maxima
-        for (int x = 0; x < numTotalThreads/(size/blockDim.x); x++) {
-            if (thread_maxima[x] > max) {
-                max = thread_maxima[x];
-            }
-        }
-
-        //The largest value of each block is placed in global_output_data
-        global_output_data[blockIdx.x] = max;
-    }
-
-
-    __syncthreads();
-
-    //find the maximum value from all blocks using one last thread
-
-    if (loopIndex == 0) {
-        float max = global_output_data[0];
-        for (int x = 0; x < numBlocks; x++) {
-            if (global_output_data[x] > max) {
-                max = global_output_data[x];
-            }
-        }
-        global_output_data[0] = max;
-    }
-    global_output_data[loopIndex] = 289.0f;
-    */
-
-    deviceResult = deviceArray;
-
-}
-
-void testVerticalOperation() {
-    //For my own sanity lets explain this.
-    //1<<20 is a notation that in this context represents
-    //a bitshift. That means that you have the bit 1 and then you shift it to the
-    //(in this case) left by 20 spaces and fill the empty space with zeros.
-    int N = 1<<18; // 1M elements
-
-    float *deviceArray, *deviceResult; //device copies
-    float hostArray, result; //host copies
-
-    hostArray = 5;
-
-    //Allocates "Unified Memory" which is accessible from both the CPU and GPU.
-    cudaError_t cudaMallocErr1 = cudaMalloc(&deviceArray, sizeof(float));
-    if (cudaMallocErr1 != cudaSuccess) {
-        cout << "CUDA Error" << endl;
-    }
-
-    //Allocates "Unified Memory" which is accessible from both the CPU and GPU.
-    cudaError_t cudaMallocErr2 = cudaMalloc(&deviceResult, sizeof(float));
-    if (cudaMallocErr2 != cudaSuccess) {
-        cout << "CUDA Error" << endl;
-    }
-
-    int blockSize = 256;
-    int numBlocks = N/blockSize;
-
-    //copy memory to device from host and print error if found
-    cudaError_t cudaMemcpy1Err = cudaMemcpy(deviceArray, hostArray, N*sizeof(float), cudaMemcpyHostToDevice);
-    if (cudaMemcpy1Err != cudaSuccess) {
-        cout << "Memcpy to Device Error: " << cudaMemcpy1Err << endl;
-    }
-
-    /*
-    //copy memory to device from host and print error if found
-    cudaMemcpy1Err = cudaMemcpy(deviceResult, result, N*sizeof(float), cudaMemcpyHostToDevice);
-    if (cudaMemcpy1Err != cudaSuccess) {
-        cout << "Memcpy to Device Error: " << cudaMemcpy1Err << endl;
-    }
-    */
-
-    verticalOperation<<<1, 1, N*sizeof(float)>>>(N, deviceArray, deviceResult);
-
-    //copy memory to host from device and print error if found
-    cudaError_t cudaMemcpy2Err = cudaMemcpy(result, deviceResult, N*sizeof(float), cudaMemcpyDeviceToHost);
-    if (cudaMemcpy2Err != cudaSuccess) {
-        cout << "Memcpy to Host Error: " << cudaMemcpy2Err << endl;
-    }
-
-    //Forces CPU to wait for GPU to finish before accessing
-    cudaDeviceSynchronize();
-
-    cout << "Largest value in hostArray: " << result << endl;
-
-    cout << "Done!" << endl;
-
-    // Free memory
-    cudaFree(deviceArray);
-    cudaFree(deviceResult);
-}
-
-
 int main() {
 
-    //Runs test for verticalOperation kernal on GPU
-    testVerticalOperation();
+    int N = 1<<20;
+    float *x, *y;
 
-    //cudaDeviceSynchronize();
+    // Allocate Unified Memory – accessible from CPU or GPU
+    cudaMallocManaged(&x, N*sizeof(float));
+    cudaMallocManaged(&y, N*sizeof(float));
+
+    // initialize x and y arrays on the host
+    for (int i = 0; i < N; i++) {
+        x[i] = 1.0f;
+        y[i] = 2.0f;
+    }
+
+    // Run kernel on 1M elements on the GPU
+    add<<<1, 1>>>(N, x, y);
+
+    // Wait for GPU to finish before accessing on host
+    cudaDeviceSynchronize();
+
+    // Check for errors (all values should be 3.0f)
+    float maxError = 0.0f;
+    for (int i = 0; i < N; i++)
+        maxError = fmax(maxError, fabs(y[i]-3.0f));
+    std::cout << "Max error: " << maxError << std::endl;
+
+    // Free memory
+    cudaFree(x);
+    cudaFree(y);
+    
     return 0;
-
 }
