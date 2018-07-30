41b88b06d3ab837a6e03ccc31bcd3ca2b14c4244
Carson Woods <cwoods289@gmail.com>
2018-07-29 20:19:17 -0400

    Finish outlining cuda reduction

--- max.cu
+++ max.cu
@@ -4,39 +4,52 @@
 using namespace std;
 
 __global__ void verticalOperation(int size, float *deviceArray, float *deviceResult) {
-    
+
     int numBlocks = gridDim.x;
     int numTotalThreads = gridDim.x * blockDim.x;
     int thread_index = blockIdx.x * blockDim.x + threadIdx.x;
-      
-    
-    float max = deviceArray[0];
-    for (int x = 0; x < size; x++) {
-        if (deviceArray[x] > max) {
-            max = deviceArray[x];
+    int blockSize = blockDim.x; //NOTE: this would also be the amount of values in shared memory (or it should be anyways)
+
+    //allocated shared memory to reduce global memory access overhead
+    extern __shared__ float sdata[];
+
+    //move each value from deviceArray pointer into shared_memory_array
+    sdata[threadIdx.x] = deviceArray[thread_index];
+
+    __syncthreads();
+
+    //stride is currently the length of the unsorted array that still needs to be compared
+    for (int stride = blockSize; stride => 1; stride /= 2) {
+        if (threadIdx.x < stride/2) {
+            if (sdata[threadIdx.x + (stride/2)] > sdata[threadIdx.x]) {
+                sdata[threadIdx.x] = sdata[threadIdx.x + (stride/2)];
+            }
+        }
+        __syncthreads();
+    }
+
+    if (threadIdx.x == 0) { deviceArray[blockIdx.x] = sdata[0]; }
+
+    //stride is currently the length of the unsorted array that still needs to be compared
+    for (int stride = gridDim.x; stride => 1; stride /= 2) {
+        if (index < stride/2) {
+            if (sdata[index + (stride/2)] > sdata[index]) {
+                sdata[index] = sdata[index + (stride/2)];
+            }
         }
     }
+
     
-    deviceResult[0] = max;
-   
-    
-    extern __shared__ float shared_memory_array[];
-
-    shared_memory_array[threadIdx.x] = deviceArray[thread_index];
-
-
-
-
 
     /*
     extern __shared__ float thread_maxima[];
-    
+
     //Sets each thread's starting point in the deviceArray
     int loopIndex = (size/numTotalThreads) * thread_index;
 
     //sets thread_max to first element in array for initial comparison
     float thread_max = deviceArray[loopIndex];
-    
+
     //iterate across a threads domain until maximum is found
     //Loop operates as follows:
     //Each thread has its starting position set by loopIndex.
@@ -47,18 +60,18 @@ __global__ void verticalOperation(int size, float *deviceArray, float *deviceRes
             thread_max = deviceArray[loopIndex + i];
         }
     }
-    
 
-    
+
+
     //max for each thread is placed into thread_maxmia for next comparison
     //NOTE: Since thread_maxima is shared memory thread_id is used.
     //      Shared Memory is only shared across a single block so index isn't used.
     thread_maxima[thread_index] = thread_max;
-    
+
     //Threads are synced to ensure all comparisons that need to be made are done.
     __syncthreads();
-    
-    
+
+
     //find the maximum value from all threads in a single block
     //appends that value to block_maximum[]
     if (threadIdx.x == 0) {
@@ -74,9 +87,9 @@ __global__ void verticalOperation(int size, float *deviceArray, float *deviceRes
         //The largest value of each block is placed in deviceArray
         deviceResult[blockIdx.x] = max;
     }
-    
+
     //find the maximum value from all blocks using one last thread
-        
+
     if (loopIndex == 0) {
         float max = deviceResult[0];
         for (int x = 1; x < numBlocks; x++) {
@@ -86,30 +99,30 @@ __global__ void verticalOperation(int size, float *deviceArray, float *deviceRes
         }
         deviceResult[0] = max;
     }
-    
+
     */
-    
+
 }
 
 void testVerticalOperation() {
 
     int number_of_values = 1 << 18;
-    
+
     int memSize = number_of_values*sizeof(float);
-    
+
     int blockSize = 256;
     int numBlocks = 1024;
-    
+
     float *deviceValue, *deviceResult; //device copies
     float initialValue[number_of_values], result[number_of_values]; //host copies
 
     for (int x = 0; x < number_of_values; x++) {
         initialValue[x] = 289.0f;
     }
-    
+
     initialValue[2] = 500.0f;
-     
-     
+
+
     //Allocates "Unified Memory" which is accessible from both the CPU and GPU.
     cudaError_t cudaMallocErr1 = cudaMalloc(&deviceValue, memSize);
     if (cudaMallocErr1 != cudaSuccess) {
@@ -120,8 +133,8 @@ void testVerticalOperation() {
     cudaError_t cudaMallocErr2 = cudaMalloc(&deviceResult, memSize);
     if (cudaMallocErr2 != cudaSuccess) {
         cout << "CUDA Error" << endl;
-    }  
-    
+    }
+
 
     //copy memory to device from host and print error if found
     cudaError_t cudaMemcpy1Err = cudaMemcpy(deviceValue, &initialValue, memSize, cudaMemcpyHostToDevice);
@@ -133,8 +146,8 @@ void testVerticalOperation() {
 
     //Forces CPU to wait for GPU to finish before accessing
     cudaDeviceSynchronize();
-    
-        
+
+
     //copy memory to host from device and print error if found
     cudaError_t cudaMemcpy2Err = cudaMemcpy(&result, deviceResult, memSize, cudaMemcpyDeviceToHost);
     if (cudaMemcpy2Err != cudaSuccess) {
@@ -155,12 +168,12 @@ int main() {
 
     //Runs test for verticalOperation kernal on GPU
     testVerticalOperation();
-  
 
 
 
 
-    //cudaDeviceSynchronize(); 
+
+    //cudaDeviceSynchronize();
     return 0;
 
 }
