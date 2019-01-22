c6cb3b87953d13056f79f63a87291780c4c7c96b
carsonwoods <cwoods289@gmail.com>
2019-01-21 23:43:36 -0500

    Update add.cu for better parallelism

--- add.cu
+++ add.cu
@@ -1,77 +1,56 @@
 #include <iostream>
-#include <math.h>
+#include <stdio.h>
 
 using namespace std;
 
+__global__ void add(float *dX, float *dY) {
 
-__global__
-void add(int n, float *x, float *y) {
-    for (int i = 0; i < n; i++) {
-        y[i] = x[i] + y[i];
-    }
-}
+    // contains the index of the current thread in the block
+    int index = blockIdx.x * blockDim.x + threadIdx.x;
+    
+    // contains the number of threads in a block
+    // int stride = blockDim.x * gridDim.x
 
-
-//I don't relly know what to call this function so this describes its
-//functionality and it will be named add2 because its basically add + extra stuff
-//z = alpha*x + beta* y
-//where z, x, and y are vectors of length N, and alpha and beta are scalars.
-__global__
-void add2(int n, float *x, float *y, float a, float b) {
-     float *z = new float[n];
-     for (int i = 0; i < n; i++) {
-         z[i] = (a * x[i]) + (b * y[i]);
-     }
+    dY[index] = dX[index] + dY[index];
+    
 }
 
 
 int main() {
-
-    //For my own sanity lets explain this.
-    //1<<20 is a notation that in this context represents
-    //a bitshift. That means that you have the bit 1 and then you shift it to the
-    //(in this case) left by 20 spaces and fill the empty space with zeros.
-    int N = 1<<20; // 1M elements
-
-    float *x, *y;
-
-    //Allocates "Unified Memory" which is accessible from both the CPU and GPU.
-    cudaError_t cudaMallocErr1 = cudaMallocManaged(&x, N*sizeof(float));
-    if (cudaMallocErr1 != cudaSuccess) {
-	    cout << "CUDA Error" << endl;
-    }
-    cudaError_t cudaMallocErr2 = cudaMallocManaged(&y, N*sizeof(float));
-    if (cudaMallocErr2 != cudaSuccess) {
-        cout << "CUDA Error" << endl;
-    }  	 
-
-    //initialize x and y arrays on the host
+    
+    int N = 256;
+    int memSize = N*sizeof(float);
+    
+    float x[N], y[N], z[N];
+    
+    float *dX, *dY;
+    
     for (int i = 0; i < N; i++) {
         x[i] = 1.0f;
-        y[i] = 2.0f;
+        y[i] = 50.0f;
     }
+    
+    cudaMalloc(&dX, memSize);
+    cudaMalloc(&dY, memSize);
+    
+    cudaMemcpy(dX, &x, memSize, cudaMemcpyHostToDevice);
+    cudaMemcpy(dY, &y, memSize, cudaMemcpyHostToDevice);
 
-    //Runs cuda kernel on 1M elements on the GPU
-    add<<<1, 1>>>(N, x, y);
-    add2<<<1, 1>>>(N, x, y, 4.0, 5.0);
-   
-    cout << "Done!" << endl;
-
-    //Forces CPU to wait for GPU to finish before accessing
+    
+    
+    add<<<1, 50>>>(dX, dY);
+    
     cudaDeviceSynchronize();
-
-    // Check for errors (all values should be 3.0f)
-    //NOTE: This does not work for any function other than add()
-    //NOTE: This is a massive performance hit. Will eventually delegate to GPU.
-    float maxError = 0.0f;
+    
+    cudaMemcpy(&z, dY, memSize, cudaMemcpyDeviceToHost);
+    
+    cudaFree(dX);
+    cudaFree(dY);
+    
     for (int i = 0; i < N; i++) {
-        maxError = fmax(maxError, fabs(y[i]-3.0f));
+        cout << z[i] << endl;
     }
-    std::cout << "Max error: " << maxError << std::endl;
-
-    // Free memory
-    cudaFree(x);
-    cudaFree(y);
-
+    
     return 0;
-} 
+    
+}
