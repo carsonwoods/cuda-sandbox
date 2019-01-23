b86a9f84f4aceb1ddcbcee7fc40d6819a7e2ba0d
carsonwoods <cwoods289@gmail.com>
2019-01-23 04:19:04 +0000

    Improve parallelism

--- add.cu
+++ add.cu
@@ -3,23 +3,42 @@
 
 using namespace std;
 
-__global__ void add(float *dX, float *dY) {
+__global__ void add(float *dX, float *dY, int N) {
 
     // contains the index of the current thread in the block
     int index = blockIdx.x * blockDim.x + threadIdx.x;
     int stride = blockDim.x * gridDim.x;
 
+    int arraySize = N;
+    
 
-    int valuesPerThread = 1;
+    int valuesPerThread;
 
+    if (stride < arraySize) {
+    	int remainder = arraySize % stride;
+	if (remainder == 0) {
+	    valuesPerThread = arraySize / stride;
+	} else {
+	    // This is not perfectly optimized, but it will do for now.
+	    valuesPerThread = floor(arraySize / stride) + remainder;
+	}
+
+    } else {
+    	valuesPerThread = 1;
+    }
+
+    int correctedIndex = index-1;
+
+    if (index != 1) {
+    	correctedIndex = index + valuesPerThread;
+    } else {
+	correctedIndex = index;
+    } 
 
     // Each threads will iterate through all assigned values
     for (int i = 0; i < valuesPerThread; i++) {
-        dY[index+i] = dX[index+i] + dY[index+i];
-    }
-    
-    dY[index] = stride; 
-    
+        dY[correctedIndex+i] = dX[correctedIndex+i] + dY[correctedIndex+i];
+    } 
 }
 
 
@@ -30,8 +49,9 @@ int main() {
     
     float x[N], y[N], z[N];
     
-    float *dX, *dY;
-    
+    float *dX = (float *) malloc(memSize);
+    float *dY = (float *) malloc(memSize);
+
     for (int i = 0; i < N; i++) {
         x[i] = 1.0f;
         y[i] = 40.0f;
@@ -45,13 +65,15 @@ int main() {
 
     
     
-    add<<<1, 50>>>(dX, dY);
+    add<<<1, 50>>>(dX, dY, N);
     
     cudaDeviceSynchronize();
     
     cudaMemcpy(&z, dY, memSize, cudaMemcpyDeviceToHost);
     
-    cout << z[0] << endl;
+    for (int i = 0; i < N; i++) {
+    	cout << z[i] << " - " << i << endl;
+    }
 
     cudaFree(dX);
     cudaFree(dY);
